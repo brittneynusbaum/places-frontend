dc056dc50880705de8d8df155eb90da2399004f6
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-10 23:40:00 -0700

    adds create action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,18 +4,26 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
-      places: []
+      places: [],
+      newPlace: {}
     };
   },
   created: function () {
-    this.indexProducts()
+    this.indexPlaces()
   },
   methods: {
-    indexProducts: function () {
+    indexPlaces: function () {
       console.log('showing all products')
       axios.get('/places').then(response => {
         console.log(response.data)
         this.places = response.data
+      });
+    },
+    createPlace: function () {
+      console.log('creating product')
+      axios.post('/places', this.newPlace).then(response => {
+        console.log(response.data)
+        this.newPlace = {}
       });
     }
   },
@@ -25,6 +33,12 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h3>Create new place</h3>
+    <p>Name: <input v-model="this.newPlace.name"></p>
+    <p>Address: <input v-model="this.newPlace.address"></p>
+    <button v-on:click="createPlace()">Save product</button>
+    <hr>
+    <h3>All places</h3>
     <div v-for="place in places" v-bind:key="place.id">
       <p>{{ place.name }}</p>
       </div>
