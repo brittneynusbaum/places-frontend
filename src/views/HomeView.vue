b6b64f498ffe590576bea27d1d854827652eb14b
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-10 23:18:13 -0700

    adds index action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,34 @@
-<script>
+<script>import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      places: []
     };
   },
-  created: function () { },
-  methods: {},
+  created: function () {
+    this.indexProducts()
+  },
+  methods: {
+    indexProducts: function () {
+      console.log('showing all products')
+      axios.get('/places').then(response => {
+        console.log(response.data)
+        this.places = response.data
+      });
+    }
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-  </div>
+    <div v-for="place in places" v-bind:key="place.id">
+      <p>{{ place.name }}</p>
+      </div>
+</div>
 </template>
 
 <style>
